90d1549f91fc7cf76786e7f2423962db41dcee5e
Robert Roschat <nathris@Roberts-MacBook-Pro.local>
2017-12-17 02:13:33 -0800

    sign type

--- signs.js
+++ signs.js
@@ -9,6 +9,7 @@ window.onload = function() {
         format: 'letter'
     });
 
+    addSign();
 }
 
 function drawBackground(x,y) {
@@ -19,7 +20,7 @@ function drawBackground(x,y) {
     doc.triangle(50+x*w,69+y*h,55+x*w,74+y*h,50+x*w,79+y*h,'F')
 }
 
-function generateSign(x, y, productBrand, productName, productDescription, price, unitPrice, UOM, UPC, endDate) {
+function generateSign(x, y, productBrand, productName, productDescription, price, unitPrice, UOM, UPC, endDate, saleType) {
     doc.setFont('arial');
 
     doc.setFontStyle("bold");
@@ -35,7 +36,7 @@ function generateSign(x, y, productBrand, productName, productDescription, price
     doc.setFontSize(66);
     doc.text((p[0]) ? p[0]:"0", 82+x*w, 64+y*h, align="right");
     doc.setFontSize(33);
-    doc.text((p[1]) ? p[1]:"00", 83+x*w, 55+y*h);
+    doc.text((p[1]) ? p[1]:"00", 83+x*w, 56+y*h);
 
     doc.setFontSize(12);
     doc.setFontStyle('bold');
@@ -44,7 +45,26 @@ function generateSign(x, y, productBrand, productName, productDescription, price
     doc.setFontSize(9);
     doc.setFontStyle('normal');
     doc.text("Unit Price: $ " + unitPrice + " / " + UOM, 58+x*w, 74+y*h);
-    doc.text(UPC + "   " + endDate, 57+x*w, 78+y*h);
+    doc.text(UPC, 55+x*w, 78+y*h);
+    doc.text(endDate, 79+x*w, 78+y*h);
+
+    switch(saleType) {
+        case 0://ad
+            doc.setFillColor(0);
+            doc.triangle(75+x*w,77+y*h,76+x*w,76+y*h,77+x*w,77+y*h,'F');
+            doc.triangle(75+x*w,77+y*h,76+x*w,78+y*h,77+x*w,77+y*h,'F');
+        break;
+        case 1://ed
+            doc.text("ED", 74+x*w, 78+y*h);
+        break;
+        case 2://pc
+            doc.text("PC", 74+x*w, 78+y*h);
+        break;
+        case 3://ts
+            doc.text("TS", 74+x*w, 78+y*h);        
+        break;
+    }
+    
 }
 
 function addSign() {
@@ -58,12 +78,12 @@ function addSign() {
 
     endDate = document.getElementById("endDate").valueAsDate;
     end = (endDate.getUTCMonth()+1) + "/" + endDate.getUTCDate() + "/" + endDate.getUTCFullYear();
-
+    sale = document.getElementById("signType").selectedIndex;
     showBackground = document.getElementById("bgCheck").checked;
     for(i=0; i<3;i++){
         for(j=0; j<3;j++){
             if(showBackground) drawBackground(i,j);
-            generateSign(i, j, brand, product, description, price, unit, uom, upc, end);
+            generateSign(i, j, brand, product, description, price, unit, uom, upc, end,sale);
         }
     }
     
