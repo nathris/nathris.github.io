6de55b8543e3e75789c2c6e8385fb904c902830f
nathris <nathris@example.com>
2017-12-18 00:10:17 -0800

    formatting

--- signs.js
+++ signs.js
@@ -1,5 +1,5 @@
 var doc;
-var w = 88;
+var w = 89;
 var h = 57;
 
 window.onload = function() {
@@ -23,31 +23,38 @@ window.onload = function() {
 
 function drawBackground(x,y) {
     doc.setFillColor(255,0,0);
-    doc.rect(10+x*w, 26+y*h, 86, 10,'F');
+    doc.rect(5+x*w, 24+y*h, 86, 10,'F');
     doc.setFillColor(0,150,0);
-    doc.rect(10+x*w, 69+y*h, 40, 10,'F');
-    doc.triangle(50+x*w,69+y*h,55+x*w,74+y*h,50+x*w,79+y*h,'F')
+    doc.rect(5+x*w, 69+y*h, 41, 10,'F');
+    doc.triangle(46+x*w,69+y*h,52+x*w,74+y*h,46+x*w,79+y*h,'F')
 }
 
 function generateSign(x, y, productBrand, productName, productDescription, price, unitPrice, UOM, UPC, endDate, saleType, youSave, options) {
-    doc.setFont('arial');
+    doc.setFont('helvetica');
 
-    lm = 10.5;
+    lm = 8;
 
     doc.setFontStyle("bold");
-    doc.setFontSize(14.5);
+    console.log(productName.length);
+    if(productName.length < 14) doc.setFontSize(15);
+    else if (productName.length < 20) doc.setFontSize(14);
+    else doc.setFontSize(12);
     doc.text(productName, lm+x*w, 46+y*h);
 
     doc.setFontStyle("normal");
     doc.setFontSize(10);
     doc.text(productBrand, lm+x*w, 41+y*h);
-    doc.text(productDescription, lm+x*w, 54+y*h);
+    doc.text(productDescription, lm+x*w, 50+y*h);
 
-    p = price.split(".");    
-    doc.setFontSize(69);
-    doc.text((p[0]) ? p[0]:"0", lm+67+x*w, 64+y*h, align="right");
-    doc.setFontSize(34.5);
-    doc.text((p[1]) ? p[1]:"00", lm+68+x*w, 55+y*h);
+    p = price.split(".");  
+    if(p[0].length >= 3)  doc.setFontSize(60);
+    else if(p[0].length == 2)  doc.setFontSize(66);
+    else doc.setFontSize(68);
+    doc.text((p[0]) ? p[0]:"0", lm+68+x*w, 64+y*h, align="right");
+    if(p[0].length >= 3)  doc.setFontSize(30);
+    else if(p[0].length == 2)  doc.setFontSize(33);
+    else doc.setFontSize(34);
+    doc.text((p[1]) ? p[1]:"00", lm+68+x*w, p[0].length >= 3 ? 56+y*h : 55+y*h);
 
     doc.setFontSize(11);
     doc.setFontStyle('bold');
@@ -59,9 +66,10 @@ function generateSign(x, y, productBrand, productName, productDescription, price
 
     doc.setFontSize(8);
     doc.setFontStyle('normal');
-    doc.text("Unit Price: $  " + unitPrice + " / " + UOM, lm+47+x*w, 73.5+y*h);
+    doc.text("Unit Price: $  ", lm+46+x*w, 73.5+y*h);
+    doc.text(unitPrice + " / " + UOM, lm+80+x*w, 73.5+y*h, align="right")
     doc.text(UPC, lm+45+x*w, 77.5+y*h);
-    doc.text(endDate, lm+69+x*w, 77.5+y*h);
+    doc.text(endDate, lm+81+x*w, 77.5+y*h, align="right");
 
     if(options.thisVar) {
         doc.setFontSize(14);
@@ -79,8 +87,8 @@ function generateSign(x, y, productBrand, productName, productDescription, price
     switch(saleType) {
         case 0://ad
             doc.setFillColor(0);
-            doc.triangle(lm+64+x*w,77+y*h,lm+65+x*w,75.75+y*h,lm+66+x*w,77+y*h,'F');
-            doc.triangle(lm+64+x*w,77+y*h,lm+65+x*w,78.25+y*h,lm+66+x*w,77+y*h,'F');
+            doc.triangle(lm+63+x*w,76.5+y*h,lm+64+x*w,75.25+y*h,lm+65+x*w,76.5+y*h,'F');
+            doc.triangle(lm+63+x*w,76.5+y*h,lm+64+x*w,77.75+y*h,lm+65+x*w,76.5+y*h,'F');
         break;
         case 1://ed
             doc.text("ED", lm+63+x*w, 78+y*h);
